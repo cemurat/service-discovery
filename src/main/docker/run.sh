7fd403ad83672f30d6e2e588e99d9fc2fe8167fb
M.Murat <mmuratakbiyik@gmail.com>
2019-05-28 20:39:49 +0300

    dockerfile update

--- src/main/docker/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-echo "********************************************************"
-echo "Starting Authentication Service                           "
-echo "********************************************************"
-java -jar /usr/local/authenticationservice/@project.build.finalName@.jar
